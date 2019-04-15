a6bba61fa69e2d9e6810e93daa2f7184d7c5a7d9
rohan <rshanker779@gmail.com>
2019-04-15 21:03:09 +0100

    Adding hostname, username functions and some extra methods to logger

--- logger.py
+++ logger.py
@@ -1,5 +1,27 @@
 import logging
-def get_logger(level=logging.DEBUG):
+
+
+def get_logger(level: int = logging.DEBUG) -> logging.Logger:
     logging.basicConfig(level=level)
     logger = logging.getLogger(__name__)
+    logger.addHandler(get_sys_out_handler())
     return logger
+
+
+def get_default_formatter() -> logging.Formatter:
+    return logging.Formatter('%(asctime)s - %(funcName)s - %(levelno)s - %(levelname)s - %(message)s')
+
+
+def get_sys_out_handler() -> logging.Handler:
+    handler = logging.StreamHandler()
+    handler.setFormatter(get_default_formatter())
+    return handler
+
+
+def clear_handlers(logger: logging.Logger):
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+
+if __name__ == '__main__':
+    lgr = get_logger()
--- utilities.py
+++ utilities.py
@@ -1,8 +1,12 @@
 import os
 import time
 from functools import wraps
+import socket
+import getpass
+from typing import Dict, Union
 
-def get_reformatted_headers(filepath):
+
+def get_reformatted_headers(filepath: Union[str, bytes]) -> Dict:
     header_dict = {}
     with open(filepath, 'r') as f:
         for row in f:
@@ -10,10 +14,18 @@ def get_reformatted_headers(filepath):
     return header_dict
 
 
-def make_dirs(directory):
+def make_dirs(directory: Union[str, bytes]) -> Union[str, bytes]:
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
+
+
+def get_hostname() -> str:
+    return socket.gethostname()
+
+
+def get_username() -> str:
+    return getpass.getuser()
 
 
 def time_it(f):
@@ -23,9 +35,12 @@ def time_it(f):
         f(*args, **kwargs)
         elapsed = time.perf_counter() - s
         print(f"{f.__name__} exectuted in {elapsed:2f} seconds")
+
     return wrapper_func
 
 
 if __name__ == '__main__':
+    x = get_hostname()
+    y = get_username()
     x = get_reformatted_headers(os.path.join('espn_golf', 'headers.txt'))
     print()
