2636f774035fdedf46f27eb5cd4332c1cd45faf2
rohan <rshanker779@gmail.com>
2019-06-03 21:27:55 +0100

    Refactoring to set up module correctly

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="rshanker779_common",
+      version='1.0.0',
+      author="rshanker779",
+      author_email="rshanker779@gmail.com",
+      description="Common utilities",
+      python_requires='>=3.5',
+      packages=find_packages(),
+      )
