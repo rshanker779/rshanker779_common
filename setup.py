a8f2a6417f8383fe1a1d0a9feec366209f62f35f
rohan <rshanker779@gmail.com>
2019-06-11 21:42:02 +0100

    Changing python version and pre commit yaml

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author="rshanker779",
     author_email="rshanker779@gmail.com",
     description="Common utilities",
-    python_requires=">=3.5",
-    install_requires=["black"],
+    python_requires=">=3.7",
+    install_requires=["black", "pre-commit"],
     packages=find_packages(),
     test_suite="tests.utilities_test",
 )
