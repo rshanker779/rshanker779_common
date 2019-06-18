740c62d0dc677e2a9a568c4d3afa336bf7913417
rohan <rshanker779@gmail.com>
2019-06-18 23:14:07 +0100

    Adding function to add inits

--- rshanker779_common/utilities.py
+++ rshanker779_common/utilities.py
@@ -4,6 +4,9 @@ from functools import wraps
 import socket
 import getpass
 from typing import Dict, Union
+from rshanker779_common.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 def get_reformatted_headers(filepath: Union[str, bytes]) -> Dict:
@@ -18,6 +21,17 @@ def make_dirs(directory: Union[str, bytes]) -> Union[str, bytes]:
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
+
+
+def add_init_files(filepath: Union[str, bytes]):
+    for root, _, files in os.walk(filepath):
+        # We need an init- if we have .py files, aren't in base dir (setup.py exists) and not already one
+        needs_init_file = any(i for i in files if i.endswith(".py") and i != "setup.py")
+        has_init_file = any(i for i in files if i == "__init__.py")
+        if needs_init_file and not has_init_file:
+            logger.info("Adding init file to directory %s", root)
+            with open(os.path.join(root, "__init__.py"), "w") as f:
+                pass
 
 
 def get_hostname() -> str:
@@ -50,5 +64,5 @@ def time_it(f):
 if __name__ == "__main__":
     x = get_hostname()
     y = get_username()
-    x = get_reformatted_headers(os.path.join("espn_golf", "headers.txt"))
+    add_init_files(os.path.join("/home/rohan/Documents/cellular-automata"))
     print()
