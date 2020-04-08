dee2cf9349486ebd448799f6fd8a0a8ffa19953d
rohan <rshanker779@gmail.com>
2020-04-08 15:20:50 +0100

    Revert "Bump version: 0.0.2 → 0.1.0"
    This reverts commit 46f15f2fa54c093c7f2e9735c7661266f7e2591f.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rshanker779_common",
-    version="0.1.0",
+    version="0.0.2",
     author="rshanker779",
     author_email="rshanker779@gmail.com",
     description="Common utilities",
