8d670650e36d7b0be64776df07f6498c56f94477
rohan <rshanker779@gmail.com>
2020-04-08 15:39:03 +0100

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rshanker779_common",
-    version="0.0.4",
+    version="0.0.5",
     author="rshanker779",
     author_email="rshanker779@gmail.com",
     description="Common utilities",
